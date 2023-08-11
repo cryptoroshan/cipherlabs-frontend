0ee85a882d5ca2a4e9ec928651b60fc348e86ded
tinyphoniex <royalguy1031@gmail.com>
2023-08-11 10:14:08 +0900

    change app background image

--- src/layouts/AppLayout/index.jsx
+++ src/layouts/AppLayout/index.jsx
@@ -5,7 +5,7 @@ import Footer from './footer'
 
 const AppLayout = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-between bg-[url('/imgs/landing/app_background.png')] bg-black bg-cover overflow-auto overscroll-y-scroll no-scrollbar">
+    <div className="w-full h-screen flex flex-col justify-between bg-[url('/imgs/landing/app_background.svg')] bg-black bg-cover overflow-auto overscroll-y-scroll no-scrollbar">
       <Header />
       <main>
         <Outlet />
