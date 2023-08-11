502d4b880eb6af4e5a2512be9f5fdcf628ffcc20
tinyphoniex <royalguy1031@gmail.com>
2023-08-11 19:35:43 +0900

    change background image

--- src/layouts/LandingLayout/index.jsx
+++ src/layouts/LandingLayout/index.jsx
@@ -5,7 +5,7 @@ import Footer from './footer'
 
 const LandingLayout = () => {
   return (
-    <div className="w-full min-h-screen flex flex-col justify-between bg-[url('/imgs/landing/landing_background.svg')] bg-cover bg-no-repeat px-[22px] py-5">
+    <div className="w-full min-h-screen flex flex-col justify-between bg-[url('/imgs/landing/landing_background.jpg')] bg-repeat-round px-[22px] py-5">
       <Header />
       <main>
         <Outlet />
