f479c619d2199be803c97c2e8a1c3910068b2133
tinyphoniex <royalguy1031@gmail.com>
2023-08-12 17:03:07 +0900

    modify landing page

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,12 +1,12 @@
 const LandingPage = () => {
   return (
-    <div className="flex flex-col gap-6 items-center lg:w-2/3 mx-auto text-center">
+    <div className="flex flex-col gap-6 items-center mx-auto text-center sm:max-w-[514px] xl:max-w-[768px] 2xl:max-w-[1028px]">
       <p className="text-5xl xl:text-[64px] 2xl:text-[96px] font-black text-secondary" style={{lineHeight: 'normal'}}>
         Blockchain Craftsmen
       </p>
       <p className="text-base xl:text-2xl 2xl:text-[32px] font-medium text-primary" style={{lineHeight: 'normal'}}>
-        Specialising in Web3, we're committed to revolutionising the DeFi landscape <br/>
-        through advanced trading tools, active governance participation, and <br/>
+        Specialising in Web3, we're committed to revolutionising the DeFi landscape
+        through advanced trading tools, active governance participation, and
         bespoke software development.
       </p>
     </div>
