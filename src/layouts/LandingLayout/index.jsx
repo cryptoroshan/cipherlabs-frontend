25d2aaa32b157c66a7db4c7518a44109503d3b46
tinyphoniex <royalguy1031@gmail.com>
2023-08-15 23:43:48 +0900

    add og image

--- src/layouts/LandingLayout/index.jsx
+++ src/layouts/LandingLayout/index.jsx
@@ -1,18 +1,33 @@
-import { Outlet } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
 
-import Header from './header'
-import Footer from './footer'
+import Header from "./header";
+import Footer from "./footer";
 
 const LandingLayout = () => {
-  return (
-    <div className="w-full min-h-screen flex flex-col justify-between bg-[url('/imgs/landing/landing_background.jpg')] bg-cover bg-no-repeat bg-center px-[22px] py-5">
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  )
-}
+  const [backgroundImage, setBackgroundImage] = useState(null);
 
-export default LandingLayout
+  useEffect(() => {
+    const imageLoader = new Image();
+    imageLoader.src = "/imgs/landing/landing_background.jpg";
+    imageLoader.onload = () => {
+      console.log("loaded!!!");
+      setBackgroundImage("/imgs/landing/landing_background.jpg");
+    };
+  }, []);
+  return (
+    <>
+      {backgroundImage !== null && (
+        <div className="w-full min-h-screen flex flex-col justify-between bg-[url('/imgs/landing/landing_background.jpg')] bg-cover bg-no-repeat bg-center px-[22px] py-5">
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default LandingLayout;
