afd068253187d3546f9c0a1a64dadf6be578b343
tinyphoniex <royalguy1031@gmail.com>
2023-09-01 17:51:42 +0900

    modify text color

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -7,14 +7,14 @@ const LandingPage = () => {
   }, [])
 
   return (
-    <div className="flex flex-col gap-6 items-center mx-auto text-center xs:max-w-[250px] sm:max-w-[514px] xl:max-w-[768px] 2xl:max-w-[1028px] 3xl:max-w-[1128px] 4xl:max-w-[1228px] 5xl:max-w-[1328px] 6xl:max-w-[1428px]">
-      <p className="text-5xl xl:text-[64px] 2xl:text-[96px] 3xl:text-[96px] 4xl:text-[96px] 5xl:text-[96px] 6xl:text-[96px] font-black text-secondary" style={{
+    <div className="flex flex-col gap-6 items-center mx-auto text-center">
+      <p className="text-5xl xl:text-[64px] 2xl:text-[96px] 3xl:text-[96px] 4xl:text-[96px] 5xl:text-[96px] 6xl:text-[96px] font-black text-secondary xs:max-w-[250px] sm:max-w-[514px] xl:max-w-[768px] 2xl:max-w-[1028px] 3xl:max-w-[5000px]" style={{
         lineHeight: 'normal',
         textShadow: '0px 0px 100px #000'
       }}>
         Blockchain Craftsmen
       </p>
-      <p className="text-base xl:text-2xl 2xl:text-[32px] font-medium text-primary" style={{
+      <p className="text-base xl:text-2xl 2xl:text-[32px] font-medium text-primary xs:max-w-[250px] sm:max-w-[514px] xl:max-w-[768px] 2xl:max-w-[1028px]" style={{
         lineHeight: 'normal',
         textShadow: '0px 0px 100px #000'
       }}>
